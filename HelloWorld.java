da15a6d0a9d084fb6aa1e3cce1f99529841ca879
Lucio Gonzalez Daniquen <liadaniquen@hotmail.com>
2019-03-14 08:36:35 -0600

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,8 @@
+// El legendario  Hola Mundo
 public class HelloWorld {
   public static void main(String[] args) {
     System.out.println("Hello World!");
   }
 }
+
+/*Sirvase de leer el  apartado Intro Para entender este Codigo*/
