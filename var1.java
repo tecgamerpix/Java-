02b3f300866ad9ae2edd8edf40bd0f39391f515d
Lucio Gonzalez Daniquen <liadaniquen@hotmail.com>
2019-03-19 08:20:51 -0600

    Create var1.java
    Muestra como se declaran 2 variables y se depliegan en pantalla

---
+++ var1.java
@@ -0,0 +1,11 @@
+public class Creator {
+	public static void main(String[] args) {
+    // Variables Area
+    String name = "James Gosling";
+    int yearCreated = 1995;
+    //Print Area
+    System.out.println(name);
+    System.out.println(yearCreated);
+    
+	}
+}
