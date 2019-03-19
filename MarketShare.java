bb6970350f0f1f476580a976bfa748df4bc5e282
Lucio Gonzalez Daniquen <liadaniquen@hotmail.com>
2019-03-19 08:59:22 -0600

    Create MarketShare.java
    Explain  use of Double variable in java

---
+++ MarketShare.java
@@ -0,0 +1,11 @@
+public class MarketShare {
+	public static void main(String[] args) {
+   /*As of 2016, Android has 81.7 percent of the market share for mobile operating systems. Create a variable called androidShare that holds this percentage as a double.*/
+     // variable area 
+    double androidShare;
+    androidShare = 81.7;
+   // print area
+    System.out.println(androidShare);
+    
+	}
+}
