b8bc6c677b71df4c1e02a5194b9bb9a40475f59a
Lucio Gonzalez Daniquen <liadaniquen@hotmail.com>
2019-03-24 09:01:32 -0600

    Create PlusAndMinus.java

---
+++ PlusAndMinus.java
@@ -0,0 +1,15 @@
+//ejemplo de  suma y resta de variables tanto de nombre como por valor. 
+public class PlusAndMinus {
+	public static void main(String[] args) {   
+    int zebrasInZoo = 8;
+    int giraffesInZoo = 4;
+    //Total de animales 
+		int animalsInZoo= zebrasInZoo + giraffesInZoo;
+    //y si  vendemos  2 zebras
+    int numZebrasAfterTrade = zebrasInZoo - 2;
+    
+  System.out.println(animalsInZoo);
+   System.out.println(numZebrasAfterTrade);
+  }
+  
+}
