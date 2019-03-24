9bd760d1bef2764c69dffa52f15b85686038c665
Lucio Gonzalez Daniquen <liadaniquen@hotmail.com>
2019-03-24 08:37:28 -0600

    Create MyProfile.java

---
+++ MyProfile.java
@@ -0,0 +1,11 @@
+//Codigo ejemplo para guardar tipos de variables  
+public class MyProfile {
+	public static void main(String[] args) {   
+String name ="Daniquen Lucio";
+    int age = 31;
+    double desiredSalary = 3000.00;
+    char gender = 'm';
+    boolean lookingForJob = true;
+    
+	}
+}
