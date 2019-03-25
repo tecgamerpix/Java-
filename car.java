393d09136160b6dddaf8e823bd32559bd5f8e309
Lucio Gonzalez Daniquen <liadaniquen@hotmail.com>
2019-03-25 08:12:32 -0600

    Create car.java

---
+++ car.java
@@ -0,0 +1,27 @@
+//Ejemplo de una clase con diversos campos y como acceden a ellos 
+
+public class Car {
+  String color;
+  // new fields!
+  boolean isRunning;
+  int velocity;
+
+  // new parameters that correspond to the new fields
+  public Car(String carColor, boolean carRunning, int milesPerHour) {
+    color = carColor;
+    // assign new parameters to the new fields
+    isRunning = carRunning;
+    velocity = milesPerHour;
+  }
+
+  public static void main(String[] args) {
+    // new values passed into the method call
+    Car ferrari = new Car("red", true, 27);
+    Car renault = new Car("blue", false, 70);
+
+    renault.isRunning;
+    // false
+    ferrari.velocity;
+    // 27
+  }
+}
